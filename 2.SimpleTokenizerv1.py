255c2bda3697ab623f045eba1c5a9826806b85e6
bsrinivas84 <bsrinivas84@gmail.com>
2026-07-20 01:34:51 -0700

    Tokenizer

---
+++ 2.SimpleTokenizerv1.py
@@ -0,0 +1,40 @@
+import re
+
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,:;?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
+
+with open('Verdict.txt', 'r',encoding = "utf-8") as file:
+    content = file.read()
+    print(len(content)) #20479
+
+
+result = re.split(r'([,:;?_!"()\']|--|\s)', content)
+preprocessed = [item.strip() for item in result if item.strip()]
+print(len(preprocessed)) #4500 instead of 4690
+
+preprocessed = sorted(set(preprocessed))
+vocab_size = len(preprocessed)
+print(vocab_size) # 1212 instead of 1130
+
+vocab = {word: idx for idx, word in enumerate(preprocessed)}
+tokenizer = SimpleTokenizerV1(vocab)
+
+print(tokenizer.decode(tokenizer.encode("I found the couple at tea beneath their palm-trees")))
