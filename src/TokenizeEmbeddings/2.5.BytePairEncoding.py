ab0b975485819c423baf2bab29b49becb2846761
bsrinivas84 <bsrinivas84@gmail.com>
2026-07-20 23:25:26 -0700

    Data Sampling

---
+++ src/TokenizeEmbeddings/2.5.BytePairEncoding.py
@@ -0,0 +1,8 @@
+#Unknow words are replaced by <unk> token which makes them all look same
+#Break words into individual english characters
+
+import tiktoken #GPT 2 tokenizer
+
+tokenizer = tiktoken.get_encoding("gpt2")
+print(tokenizer.encode("Hello I found the asdadsadsad qwewqwe couple <|endoftext|> at tea beneath their palm-trees", allowed_special={"<|endoftext|>"})) #Hello I found the couple <|endoftext|> at tea beneath their palm-trees
+#50256 reprsents <|endoftext|> token
--- src/TokenizeEmbeddings/2.BytePairEncoding.py
+++
@@ -1,4 +0,0 @@
-#Unknow words are replaced by <unk> token which makes them all look same
-#Break words into individual characters
-
-import tiktoken #GPT 2 tokenizer
