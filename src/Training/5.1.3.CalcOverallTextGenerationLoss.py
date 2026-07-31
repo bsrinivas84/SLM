20862c13ea520e09582a09a9de372f06a3445172
bsrinivas84 <bsrinivas84@gmail.com>
2026-07-30 21:59:25 -0700

    Training Loss

---
+++ src/Training/5.1.3.CalcOverallTextGenerationLoss.py
@@ -0,0 +1,114 @@
+import torch
+from torch import nn
+import tiktoken
+from torch.return_types import mode
+tokenizer = tiktoken.get_encoding("gpt2")
+import PreviousChapters
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,
+    "context_length": 256,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12,
+    "drop_rate": 0.1,
+    "qkv_bias": False,
+}
+
+with open('../../data/raw/Verdict.txt', 'r',encoding = "utf-8") as file:
+    text_data = file.read()
+    text_data_length = len(text_data)
+    print(text_data_length) #20479
+
+
+print(text_data[:100]) #
+print("Total Tokens:", len(tokenizer.encode(text_data))) # Print the total number of tokens in the text data
+
+#Subset of 
+training = text_data[:10000]
+validation = text_data[10000:len(text_data)]
+
+train_ratio = 0.9
+split_index = int(text_data_length * train_ratio)
+
+training_data = text_data[:split_index]
+validation_data = text_data[split_index:text_data_length]
+print(f"Training data length: {len(training_data)}")
+print(f"Validation data length: {len(validation_data)}")
+
+train_loader = PreviousChapters.create_dataloader_v1(
+    training_data, 
+    batch_size=2, 
+    max_length=GPT_CONFIG_124M["context_length"], 
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=True, 
+    shuffle=True,
+    num_workers=0
+)
+val_loader = PreviousChapters.create_dataloader_v1(
+    validation_data, 
+    batch_size=2, 
+    max_length=GPT_CONFIG_124M["context_length"], 
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=True, 
+    shuffle=True,
+    num_workers=0
+)
+
+print("Train Loader")
+for x,y in train_loader:
+    print("Input shape:", x.shape)
+    print("Target shape:", y.shape)
+    break
+print("Validation Loader")
+for x,y in val_loader:
+    print("Input shape:", x.shape)
+    print("Target shape:", y.shape)
+    break   
+
+train_tokens = 0 
+
+for input_batch, target_batch in train_loader:
+    train_tokens += input_batch.numel()  # Count the number of tokens in the input batch
+print("Total training tokens:", train_tokens)
+
+val_tokens = 0
+for input_batch, target_batch in val_loader:
+    val_tokens += input_batch.numel()  # Count the number of tokens in the input batch
+print("Total validation tokens:", val_tokens)   
+print("Total tokens (train + validation):", train_tokens + val_tokens)
+
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
+    return loss
+
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        # Reduce the number of batches to match the total number of batches in the data loader
+        # if num_batches exceeds the number of batches in the data loader.
+        num_batches = min(num_batches, len(data_loader))
+
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            total_loss += loss.item()
+        else:
+            break
+
+    return total_loss / num_batches
+
+torch.manual_seed(123)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = PreviousChapters.GPTModel(cfg=GPT_CONFIG_124M).to(device)
+
+print("Training loss:", calc_loss_loader(train_loader, model, device))
+print("Validation loss:", calc_loss_loader(val_loader, model, device))
---
+++ src/Training/5.1.CalcTextGenerationLoss.py
@@ -0,0 +1,70 @@
+from PreviousChapters import GPTModel
+import torch
+import tiktoken
+
+
+GPT_CONFIG_124M = {
+	"vocab_size": 50257,
+	"context_length": 1024,
+	"emb_dim": 768,
+	"n_heads": 12,
+	"n_layers": 12,
+	"drop_rate": 0.1,
+	"qkv_bias": False,
+}
+
+
+def token_ids_to_text(token_ids, tokenizer):
+	flat = token_ids.flatten()
+	return tokenizer.decode(flat.tolist())
+
+
+torch.manual_seed(123)
+model = GPTModel(cfg=GPT_CONFIG_124M)
+model.eval()
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+inputs = torch.tensor([[16833, 3626, 6100],
+					   [40, 1107, 588]])
+
+targets = torch.tensor([[3626, 6100, 345],
+						[1107, 588, 11311]])
+
+with torch.no_grad():
+	logits = model(inputs)
+
+print("logits.shape:", logits.shape)
+
+probas = torch.softmax(logits, dim=-1)
+print("probas.shape:", probas.shape)
+
+token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+print("Token IDs:\n", token_ids)
+
+print(f"Targets batch 1: {token_ids_to_text(targets[0], tokenizer)}")
+print(f"Outputs batch 1: {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+
+text_idx = 0
+target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 1:", target_probas_1)
+
+text_idx = 1
+target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 2:", target_probas_2)
+
+# Compute logarithm of all token probabilities.
+log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+print(log_probas)
+
+avg_neg_log_proba = -1 * torch.mean(log_probas)
+print("Average Negative Log Probability:", avg_neg_log_proba)
+
+logits_flat = logits.flatten(0, 1)
+targets_flat = targets.flatten()
+
+ce_loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+print("cross_entropy:", ce_loss)
+
+loss = torch.nn.CrossEntropyLoss()(logits.view(-1, logits.size(-1)), targets.view(-1))
+print("Cross Entropy Loss:", loss)
